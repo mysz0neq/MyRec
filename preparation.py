1a512d24870769cd423e908f5d563bcd1f483f12
Mysz0neq <majchrzakszymon05@wp.pl>
2026-04-20 15:39:27 +0200

    added: fine tuning handling in tokenizer - updating dicts and sets, tokenizing data by already existing dicts

--- preparation.py
+++ preparation.py
@@ -1,5 +1,8 @@
 """Gets data ready for analysis and Machine Learning"""
 
+import warnings
+from copy import deepcopy
+
 def users_movies_sets(data:list[tuple[str|int,str|int,float]]) -> tuple[set,set]:
     """Returns sets of users and movies"""
     users=set()
@@ -10,23 +13,71 @@ def users_movies_sets(data:list[tuple[str|int,str|int,float]]) -> tuple[set,set]
     return users,movies
 
 def tokenizer(data: list[tuple[str,str,float]],
-              users:set=None,
-              movies:set=None) -> tuple[list[tuple[int,int,float]],dict,dict,dict,dict]:
+              base_users:set=None,
+              base_movies:set=None,
+              base_u2i:dict=None,
+              base_m2i:dict=None) -> tuple[list[tuple[int,int,float]],dict,dict,dict,dict,set[str],set[str]]:
     """Changes users and movies names to IDs and returns the dictionaries."""
-    if not users or not movies:
-        users,movies=users_movies_sets(data)
+    current_users,current_movies=users_movies_sets(data)
 
-    u2i={u:i for i,u in enumerate(users)}
-    i2u={i:u for i,u in enumerate(users)}
-    m2i={m:i for i,m in enumerate(movies)}
-    i2m={i:m for i,m in enumerate(movies)}
+    if not base_users and base_u2i:
+        warnings.warn("Users base dictionary specified without users set given!\nBase users set set to base dictionary keys.")
+        base_users=set([u for u in base_u2i.keys()])
 
-    new_data=[]
+    elif base_users and not base_u2i:
+        warnings.warn("Base users set specified without users dictionary given!\nBase dictionary created from base users set.")
+        base_u2i={u:i for i,u in enumerate(base_users)}
+
+    if not base_users and not base_u2i: #kiedy puszczamy po raz pierwszy - ma wypluć zbiór userów i u2i
+        print("Operating in first time run mode.\nCreated users set and dictionary.")
+        base_users=current_users
+        new_users=base_users
+        base_u2i = {u: i for i, u in enumerate(base_users)}
+
+    elif base_users and base_u2i: #fine tune - ma wypluc zbior userów i u2i
+        print("Operating in fine tuning mode.\nCreated new_users set and updated base dictionary.")
+        new_users=current_users-base_users
+        base_u2i.update({u:i for i,u in enumerate(new_users,start=len(base_u2i))})
+    else:
+        raise RuntimeError("Something failed while creating users dictionaries and sets.")
+
+
+    if not base_movies and base_m2i:
+        warnings.warn(
+            "Films base dictionary specified without filmss set given!\nBase filmss set set to base dictionary keys.")
+        base_movies = set([u for u in base_m2i.keys()])
+
+    elif base_movies and not base_m2i:
+        warnings.warn(
+            "Base films set specified without films dictionary given!\nBase dictionary created from base films set.")
+        base_m2i = {f: i for i, f in enumerate(base_movies)}
+
+    if not base_movies and not base_m2i:  # kiedy puszczamy po raz pierwszy - ma wypluć zbiór userów i u2i
+        print("Operating in first time run mode.\nCreated films set and dictionary.")
+        base_movies = current_movies
+        new_movies = base_movies
+        base_m2i = {f: i for i, f in enumerate(base_movies)}
+
+    elif base_movies and base_m2i:  # fine tune - ma wypluc zbior userów i u2i
+        print("Operating in fine tuning mode.\nCreated new_movies set and updated base dictionary.")
+        new_movies = current_movies - base_movies
+        base_m2i.update({f: i for i, f in enumerate(new_movies, start=len(base_m2i))})
+    else:
+        raise RuntimeError("Something failed while creating movies dictionaries and sets.")
+
+
+    u2i:dict[str,int]=deepcopy(base_u2i)
+    m2i:dict[str,int]=deepcopy(base_m2i)
+
+    i2u:dict[int,str]={i:u for u,i in u2i.items()}
+    i2m:dict[int,str]={i:m for m,i in m2i.items()}
+
+    tokenized_data:list[tuple[int,int,float]] = []
 
     for u,f,r in data:
-        new_data.append((u2i[u],m2i[f],r))
+        tokenized_data.append((u2i[u],m2i[f],r))
 
-    return new_data,u2i,i2u,m2i,i2m
+    return tokenized_data,u2i,i2u,m2i,i2m,current_users,current_movies
 
 import random
 
